c1946bbeb762a352ddc0821fa2d547639ab58632
veins-official <153231779+veins-official@users.noreply.github.com>
2024-04-08 20:30:28 +0300

    Add files via upload

--- scripts/core.js
+++ scripts/core.js
@@ -48,10 +48,14 @@ function collisions() {
 function lateUpdate() { objects.forEach((object) => { if (object.lateUpdate) object.lateUpdate(); }); }
 
 const FPS = 60; const timestep = 1000 / FPS;
-let last_time = 0; let delta_time = 0;
+
+let current_time, elapsed_time, last_time = 0;
 function loop() {
-    delta_time += performance.now() - last_time - timestep; last_time = performance.now(); setTimeout(loop, timestep - delta_time);
-    update(); collisions(); lateUpdate();
+    requestAnimationFrame(loop); current_time = performance.now(); elapsed_time = current_time - last_time;
+    if (elapsed_time > timestep) {
+        last_time = current_time - (elapsed_time % timestep);
+        update(); collisions(); lateUpdate();
+    }
 }
 
 loop();
